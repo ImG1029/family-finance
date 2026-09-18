bc38b43563794f9c4ec592b9ecc74991e49382d7
ImG1029 <geziel05@gmail.com>
2026-09-18 14:34:16 -0300

    refactor(backend): refactored pingController and pingControllerTest to domain directory

--- backend/src/main/java/br/com/geziel/family_finance/domain/ping/PingController.java
+++ backend/src/main/java/br/com/geziel/family_finance/domain/ping/PingController.java
@@ -1,7 +1,6 @@
-package br.com.geziel.family_finance.ping;
+package br.com.geziel.family_finance.domain.ping;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,6 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/ping")
-@CrossOrigin(origins = "http://localhost:5173")
 public class PingController {
     public record PingResponse(String message, LocalDateTime timestamp) {}
 
--- backend/src/test/java/br/com/geziel/family_finance/domain/ping/PingControllerTest.java
+++ backend/src/test/java/br/com/geziel/family_finance/domain/ping/PingControllerTest.java
@@ -1,19 +1,13 @@
-package br.com.geziel.family_finance.ping;
+package br.com.geziel.family_finance.domain.ping;
 
+import br.com.geziel.family_finance.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(PingController.class)
-public class PingControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
+public class PingControllerTest extends BaseIntegrationTest {
 
     @Test
     void shouldReturnPongAndTimestampOnPing() throws Exception {
